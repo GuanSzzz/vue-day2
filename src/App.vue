0a3b1d720e19924768bd170fa109cb4336e3ac6c
GuanSzzz <847388139@qq.com>
2022-06-28 23:38:52 +0800

    vue第二天作业，包括js作业

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="imglist">
+      <h3>图片列表区</h3>
+      <img src="./assets/01.jpg" alt="" @mouseover="mouseoverFn(0)" @click="clickFn(1,$event)"/>
+      <img src="./assets/02.jpg" alt="" @mouseover="mouseoverFn(1)" @click="clickFn(2,$event)"/>
+      <img src="./assets/03.jpg" alt="" @mouseover="mouseoverFn(2)" @click="clickFn(3,$event)"/>
+    </div>
+    <div class="show">
+      <h3>图片预览区</h3>
+      <img v-bind:src="current" alt="" style="width: 600px; height: auto" />
+    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import img1 from "./assets/01.jpg";
+import img2 from "./assets/02.jpg";
+import img3 from "./assets/03.jpg";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      current: img1,
+    };
+  },
+  methods: {
+    mouseoverFn(val) {
+      this.current = { 0: img1, 1: img2, 2: img3 }[val];
+    },
+    clickFn(val,e) { 
+      // console.log(e.target);
+      alert(`第${val}张，图片的地址是${e.target.src}`)
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  display: flex;
+}
+.imglist {
+  border: 2px solid red;
+}
+.imglist img {
+  width: 200px;
+  height: auto;
+}
+.show {
+  border: 2px solid red;
+  margin-right: 20px;
 }
 </style>
---
+++ src/reverse.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="click">翻转</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "HOLLE,VUE!",
+    };
+  },
+  methods: {
+    click() {
+      this.msg = this.msg.split("").reverse().join("");
+    },
+  },
+};
+</script>
+
+<style></style>
